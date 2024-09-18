6f6646efdbe91b8d9c37a7fb669ab35478199f5b
aries joseph tagle <ajtagle12@gmail.com>
2024-09-18 09:04:19 +0800

    added batch processing to the main branch

--- classes/FactCheck.py
+++ classes/FactCheck.py
@@ -7,31 +7,25 @@ from .Premise import Premise
 import os
 from dotenv import load_dotenv
 
-from urllib.parse import urlparse
 
 from classes.Counter import Counter
 load_dotenv()
 
 class FactCheckResult:
     def __init__(self, query, hypothesis):
-        self.premises = []
         self.query = query
         self.hypothesis = hypothesis
-        self.session = requests.Session()
-        self.session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'})
-        self.last_url = None
+        self.premiseClass = Premise(hypothesis=hypothesis)
     
     def __add_premise(self, premise, url, title, date):
-        hypothesis = self.hypothesis
-        premise_obj = Premise(premise=premise, hypothesis=hypothesis, url=url, title=title, date=date)
-        self.premises.append(premise_obj.to_json())
+        self.premiseClass.add_premise(premise=premise, url=url, title=title, date=date)
 
     def get_All_Premises(self):
         query = self.query
         results = self.__google_custom_search(query)
         if "items" in results:
             init_premises = 0
-            MAX_PREMISES = 8
+            MAX_PREMISES = 10
             for item in results["items"]:
                 if init_premises >= MAX_PREMISES:
                     break
@@ -48,18 +42,12 @@ class FactCheckResult:
 
                 snippet = item["snippet"]
                 sentences = snippet
-                # to increase the snippet
-                # page_content = self.__get_page_content(url)
-                # if page_content is not None:
-                #     # Get the middle words from the snippet
-                #     words = snippet.split()
-                #     middle_index = len(words) // 2
-                #     middle_words = words[max(0, middle_index - 2):middle_index + 3]
-                #     sentences = self.__find_text_with_context(page_content, middle_words)
                 
                 if sentences is not None and sentences.strip():
                     self.__add_premise(premise=sentences, url=url, title=title, date=date)
                     init_premises += 1
+            
+            return self.premiseClass.determine_all_relationship_premise_hypothesis()
 
     def __google_custom_search(self, query):
         counter_instance = Counter(db_file="google_calls.db", max_calls_per_day=80)
@@ -88,68 +76,12 @@ class FactCheckResult:
             # Regardless of the specific error, raise an exception with the message "Daily limit reached"
             raise Exception("Daily limit reached") from http_err
 
-        
-    
-    # def __google_fact_check(query, num):
-    #     api_key = os.getenv("GOOGLE_FACT_CHECK_API")  # Replace with your own API key
-    #     url = f"https://factchecktools.googleapis.com/v1alpha1/claims:search?query={query}&key={api_key}"
-        
-    #     response = requests.get(url)
-    #     data = response.json()
-
-    #     if 'claims' in data:
-    #         return data['claims'][:num]
-    #     else:
-    #         return []
-
-    # def __get_page_content(self, url):
-    #     try:
-    #         # Inside your method
-    #         parsed_current_url = urlparse(url)
-    #         current_base_url = f"{parsed_current_url.scheme}://{parsed_current_url.netloc}"
-
-    #         parsed_last_url = urlparse(self.last_url)
-    #         last_base_url = f"{parsed_last_url.scheme}://{parsed_last_url.netloc}"
-
-    #         if current_base_url == last_base_url:  # Compare base URLs instead of full URLs
-    #             time.sleep(0.5)
-
-    #         response = self.session.get(url, timeout=2)
-    #         self.last_url = url  # Update last_url with the full URL for future comparisons
-    #         if response.status_code == 200:
-    #             soup = BeautifulSoup(response.text, 'html.parser')
-    #             text = soup.get_text()
-    #             return text.replace('\n', ' ')
-    #         else:
-    #             print(f"Request failed with status code: {response.status_code}")
-    #             return None
-    #     except requests.exceptions.RequestException as e:
-    #         print(f"Error occurred: {e}")
-    #         return None
-
-    # def __find_text_with_context(self, text, words):
-    #     words_to_find = " ".join(words)
-    #     start_index = text.find(words_to_find)
-        
-    #     if start_index == -1:
-    #         return None
-
-    #     start_of_context = text.rfind('.', 0, start_index) + 1
-    #     end_of_context = text.find('.', start_index + len(words_to_find))
-
-    #     if start_of_context == -1:
-    #         start_of_context = 0
-    #     if end_of_context == -1:
-    #         end_of_context = len(text)
-
-    #     return text[start_of_context:end_of_context].strip()
 
     def to_json(self):
         return {
             'hypothesis': self.hypothesis,
-            'premises': self.premises
+            'premises': self.premiseClass.get_all_premises_with_relationship()
         }
     
     def get_processed_premises(self):
-        return self.premises
-
+        return self.premiseClass.get_all_premises_with_relationship() #change here
--- classes/Premise.py
+++ classes/Premise.py
@@ -2,36 +2,50 @@ from .NLISingleton import NLISingleton
 import torch
 
 class Premise:
-    def __init__(self, premise, hypothesis, url, title, date):
+    def __init__(self, hypothesis):
         # Ensure the model is initialized
         NLISingleton()  # This line ensures the model is initialized
-        self.premise = premise
-        self.url = url
-        self.title = title
-        self.date = date
-        self.relationship = self.__determine_relationship_premise_hypothesis(premise, hypothesis)
+        self.hypothesis = hypothesis
+        self.premises = []
+        self.relationship = []
     
-    def __determine_relationship_premise_hypothesis(self, premise, hypothesis):
-        model = NLISingleton.get_model()
+    def add_premise(self, premise, url, title, date):
+        temp_premise = {
+            'premise': premise,
+            'url': url,
+            'title': title,
+            'date': date
+        }
+        self.premises.append(temp_premise)
+
+    def determine_all_relationship_premise_hypothesis(self):
         tokenizer = NLISingleton.get_tokenizer()
+        model = NLISingleton.get_model()
         device = NLISingleton.get_device()
 
-        input = tokenizer(premise, hypothesis, truncation=True, return_tensors="pt")
-        output = model(input["input_ids"].to(device))  # device = "cuda:0" or "cpu"
-        prediction = torch.softmax(output["logits"][0], -1).tolist()
+        hypothesis = self.hypothesis
+        premises = [premise['premise'] for premise in self.premises]
+
+        hypotheses = [hypothesis] * len(premises)
+        inputs = tokenizer(premises, hypotheses, return_tensors='pt', padding=True, truncation=True)
+    
+        # Move inputs to the device (CPU)
+        inputs = {key: value.to(device) for key, value in inputs.items()}
+        
+        # Run batch inference
+        with torch.no_grad():  # Disable gradient calculation for inference
+            outputs = model(**inputs)
+        
+        # Get logits and apply softmax
+        logits = outputs.logits
+        probabilities = torch.softmax(logits, dim=-1)
+        
+        # Label mapping for NLI
         label_names = ["entailment", "neutral", "contradiction"]
-        prediction_dict = {name: round(float(pred) * 100, 1) for pred, name in zip(prediction, label_names)}
-
-        # Get the label with the highest probability
-        highest_label = max(prediction_dict, key=prediction_dict.get)
-
-        return highest_label
-
-    def to_json(self):
-        return {
-            'premise': self.premise,
-            'relationship': self.relationship,
-            'url': self.url,
-            'title': self.title,
-            'date': self.date
-        }
+        
+        for i,probs in probabilities:
+            label_idx = torch.argmax(probs).item()
+            self.premises[i]['relationship'] = label_names[label_idx]
+        
+    def get_all_premises_with_relationship(self):
+        return self.premises
