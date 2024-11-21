7508a887404cd54879400ebc96961e6af3998b16
aries joseph tagle <ajtagle12@gmail.com>
2024-11-21 20:46:43 +0800

    change nli model, and the llm model

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # prerequisite
 
 - download the model.safetensors in
-https://huggingface.co/MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli/tree/main 
+https://huggingface.co/iamaries/mdeberta_healthver/tree/main
 
 - put it in the nli folder
 
-
-- download the model.safetensors in
- https://huggingface.co/MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7/tree/main
-
-
-- put it in the zero-shot folder
 
 - read elastic_instruction.md
 
--- classes/Query.py
+++ classes/Query.py
@@ -42,7 +42,7 @@ class Query:
             }
 
             model = genai.GenerativeModel(
-            model_name="gemini-1.5-pro-002",
+            model_name="gemini-1.5-flash",
             generation_config=generation_config,
             system_instruction="""
             You will be given a text. Identify up to 4 individual health-related claims within the text and generate a separate, clear, and concise query for each claim. Respond with an array in the following format:
@@ -114,7 +114,7 @@ class Query:
             }
 
             model = genai.GenerativeModel(
-            model_name="gemini-1.5-pro-002",
+            model_name="gemini-1.5-flash",
             generation_config=generation_config,
             system_instruction="""
             You will be given a text. I need you to generate a clear an concise query regarding the text. Respond with a string in the following format:
